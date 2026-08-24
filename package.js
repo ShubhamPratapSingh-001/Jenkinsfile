02a56515e8f6a2cf821418aa501dfa59bccfd11b
SHUBHAM SINGH JADON <keertisingh0803@gmail.com>
2026-08-24 15:53:43 +0530

    Create package.json for Node.js application
    Initialize package.json for Jenkins Node.js application with basic scripts.

---
+++ package.js
@@ -0,0 +1,11 @@
+{
+  "name": "jenkins-node-application",
+  "version": "1.0.0",
+  "description": "Basic Node.js application built with Jenkins",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "build": "node build.js",
+    "test": "node test.js"
+  }
+}
